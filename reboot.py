0eef27ce18da5564141cd3b6e12294ffbc669020
Aeluin Chen <aeluin.chen@moxa.com>
2014-09-25 15:32:30 +0800

    Add unittest program and adjust the reboot to comply the unittest.

--- reboot.py
+++ reboot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os
+import time
 import logging
+import subprocess
 from sanji.core import Sanji
 from sanji.core import Route
 from sanji.connection.mqtt import Mqtt
@@ -9,30 +12,37 @@ from sanji.connection.mqtt import Mqtt
 
 TURN_OFF_READYLED = '/etc/init.d/showreadyled stop'
 
+# TODO: logger should be defined in sanji package?
+logger = logging.getLogger()
+
 class Reboot(Sanji):
 
-    def init(self):
-        pass
+    def init(self, bundle_env=os.getenv('BUNDLE_ENV', 'debug')):
+        self.set_to_not_ready = TURN_OFF_READYLED
+        self.call_reboot = 'reboot'
+        if bundle_env == 'debug':
+            self.set_to_not_ready = 'echo "%s"' % TURN_OFF_READYLED
+            self.call_reboot = 'echo reboot'
 
     @Route(methods='put', resource='/system/reboot')
     def put(self, message, response):
         if not hasattr(message, 'data') or 'enable' not in message.data:
-            return response(code=400, data={'message': 'Invaild Input.'})
+            return response(code=400, data={'message': 'Invalid Input.'})
 
         if message.data['enable'] == 1:
             # Response success before reboot
             response()
-
-            # Waiting for web to log out
-            sleep(5)
-            logger.debug('Turn off the ready led')
-            subprocess.call(TURN_OFF_READYLED, shell=True)
-            logger.info('Rebooting...')
-            subprocess.call('reboot', shell=True)
-            print 'reboot'
+            self.reboot()
             return
-
         return response()
+
+    def reboot(self):
+        # Waiting for web to log out
+        time.sleep(5)
+        logger.debug('Turn off the ready led')
+        subprocess.call(self.set_to_not_ready, shell=True)
+        logger.info('Rebooting...')
+        subprocess.call(self.call_reboot, shell=True)
 
 
 if __name__ == '__main__':
